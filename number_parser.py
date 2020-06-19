b38357711036b2f92b57234691c245cc39931d30
silent-x <silent.xx@gmail.com>
2020-06-19 17:08:02 +0800

    change number parsing from file name

--- number_parser.py
+++ number_parser.py
@@ -27,6 +27,21 @@ def get_number(filepath: str) -> str:
     'snis-829'
     """
     filepath = os.path.basename(filepath)
+    code_match_pattern1 = '[a-zA-Z]{2,5}[-_][0-9]{3,5}'
+    code_match_pattern2 = '([a-zA-Z]{2,5})([0-9]{3,5})'
+    re_rules1 = re.compile(code_match_pattern1, flags=re.IGNORECASE)
+    re_rules2 = re.compile(code_match_pattern2, flags=re.IGNORECASE)
+
+    file_code1 = re_rules1.findall(filepath)
+    file_code2 = re_rules2.findall(filepath)
+    if file_code1:
+        return file_code1[0]
+    elif file_code2:
+        return file_code2[0][0].upper() + '-' +"{:0>3d}".format(int(file_code2[0][1]))#file_code2[0][1]
+    else:
+        return "UNKNOWN"
+
+    #below is not used...
 
     if '-' in filepath or '_' in filepath:  # 普通提取番号 主要处理包含减号-和_的番号
         filepath = filepath.replace("_", "-")
